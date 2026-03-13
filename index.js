c5636cd84b9a35c5c391e58df41bfbb67af119fc
alicuentadealvaro-cmd <alicuentadealvaro@gmail.com>
2026-03-13 20:59:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({ 
     checkUpdate: false,
-    patchVoice: true // ESTA LÍNEA ARREGLA EL ERROR DE TU FOTO
+    patchVoice: true // <--- ESTO ES LO QUE ARREGLA EL ERROR DE TU FOTO
 });
 const http = require('http');
 
+// Servidor para que Render esté contento
 http.createServer((req, res) => res.end("Gior RPC Online")).listen(process.env.PORT || 3000);
 
 client.on('ready', async () => {
@@ -15,8 +16,8 @@ client.on('ready', async () => {
     .setType('PLAYING')
     .setState('✅ Official Reseller')
     .setDetails('Best Prices')
-    .addButton('SHOP LINK', 'https://giorshop.mysellauth.com/')
-    .addButton('JOIN DISCORD', 'https://discord.gg/UzHePBPT29');
+    .addButton('SHOP LINK', 'https://giorstore.xyz')
+    .addButton('JOIN DISCORD', 'https://discord.gg/TU_LINK');
 
   client.user.setPresence({ activities: [r] });
 });
