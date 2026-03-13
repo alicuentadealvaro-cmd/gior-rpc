19d8d23410cac587b61239b602e185b30729da81
alicuentadealvaro-cmd <alicuentadealvaro@gmail.com>
2026-03-13 21:27:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({ checkUpdate: false, patchVoice: true });
+const client = new Client({ 
+    checkUpdate: false, 
+    patchVoice: true 
+});
 const http = require('http');
 
-http.createServer((req, res) => res.end("RPC Gior Online")).listen(process.env.PORT || 3000);
+// Servidor básico para Render
+http.createServer((req, res) => {
+    res.write("RPC Gior Funcionando");
+    res.end();
+}).listen(process.env.PORT || 3000);
 
 client.on('ready', async () => {
-  console.log(`¡LOGUEADO EN ${client.user.tag}!`);
+  console.log(`¡LOGUEADO CON ÉXITO EN ${client.user.tag}!`);
+  
   const r = new (require('discord.js-selfbot-v13')).RichPresence(client)
-    .setApplicationId('1482072730785419314')
+    .setApplicationId('1482072730785419314') // Tu ID corregido
     .setType('PLAYING')
     .setState('✅ Official Reseller')
     .setDetails('Best Prices')
-    .addButton('SHOP LINK', 'https://giorshop.mysellauth.com/')
-    .addButton('JOIN DISCORD', 'https://discord.gg/UzHePBPT29');
+    .addButton('SHOP LINK', 'https://giorstore.xyz')
+    .addButton('JOIN DISCORD', 'https://discord.gg/TU_LINK');
+
   client.user.setPresence({ activities: [r] });
 });
 
